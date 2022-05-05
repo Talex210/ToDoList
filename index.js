64f0b64ecdd4a2bad6b1f04fb5e4897beb719870
Talex210 <tal.89@mail.ru>
2022-05-05 15:14:57 +0300

    Working on the design again =). Refactoring. Adding a task also on Enter NumLock. The name of the project is made in the right proportion. The checkbox is changed to a picture, but the switch is not done until the end.

--- index.js
+++ index.js
@@ -1,57 +1,60 @@
 let ul = document.getElementById('task-list');
 let taskList = [];
-taskList = JSON.parse(localStorage.getItem('tasks')) || [];
+let checkboxNumber = 0;
+checkboxNumber = JSON.parse(localStorage.getItem('checks'))
+if (checkboxNumber === null) {
+    checkboxNumber = 0;
+}
 
 // restoring an array from memory
+taskList = JSON.parse(localStorage.getItem('tasks')) || [];
 for (let i = 0; i < taskList.length; i++) {
     let li = document.createElement('li');
     li.innerHTML = taskList[i];
     ul.append(li);
 }
 
-let allInput = document.querySelectorAll('input');
+let allImg = document.querySelectorAll('img');
 let inputTask = document.getElementById('input-task');
-let checkboxNumber = 1;
+// let checkboxNumber = 1;
 let allButton = document.querySelectorAll('button');
 let allLi = document.querySelectorAll('li');
 let allSpan = document.querySelectorAll('span');
-let checkboxList = [];
+// let checkboxList = [];
 inputTask.focus();
 
 // restoring checks
-checkboxList = JSON.parse(localStorage.getItem('check')) || [];
-for (let i = 0; i < allInput.length - 1; i++) {
-    if (checkboxList[i] && allInput.length > 1) {
-        allInput[i + 1].checked = true;
+/*checkboxList = JSON.parse(localStorage.getItem('check')) || [];
+for (let i = 0; i < allImg.length - 1; i++) {
+    if (checkboxList[i] && allImg.length > 1) {
+        allImg[i].checked = true;
     }
-}
+}*/
 
 const addTask = () => {
     if (inputTask.value !== '') {
+        let task = '<img src = \'/img/notDone.png\' alt = \'not done\' id = \'checkbox' + checkboxNumber + '\' class = \'checkbox\' onclick=\'changeImg()\'/>' +
+            '<span class = \'task\'>' + inputTask.value + '</span>' +
+            '<button class = \'delete-btn\'>X</button>'
         let li = document.createElement('li');
-        li.innerHTML = '<label for = \'checkbox' + checkboxNumber + '\'></label>' +
-            '          <input type = \'checkbox\' id = \'checkbox' + checkboxNumber + '\' class = \'checkbox\'>' +
-            '          <span class = \'task\'>' + inputTask.value + '</span>' +
-            '          <button class = \'delete-btn\'>x</button>';
+        li.innerHTML = task;
         ul.append(li);
-        taskList.push('<label for = \'checkbox' + checkboxNumber + '\'></label>' +
-            '          <input type = \'checkbox\' id = \'checkbox' + checkboxNumber + '\' class = \'checkbox\'>' +
-            '          <span class = \'task\'>' + inputTask.value + '</span>' +
-            '          <button class = \'delete-btn\'>x</button>');
+        taskList.push(task);
         allButton = document.querySelectorAll('button');
         allLi = document.querySelectorAll('li');
-        checkboxNumber++;
+        ++checkboxNumber;
         inputTask.value = '';
         allSpan = document.querySelectorAll('span');
-        allInput = document.querySelectorAll('input');
+        allImg = document.querySelectorAll('img');
         localStorage.setItem('tasks', JSON.stringify(taskList));
-        del();
+        localStorage.setItem('checks', JSON.stringify(checkboxNumber));
+        // del();
         location.reload();
     }
 }
 
 document.addEventListener('keydown', function (event) {
-    if (event.code === 'Enter') {
+    if (event.code === 'Enter' || event.code === 'NumpadEnter') {
         addTask();
     }
 })
@@ -60,11 +63,22 @@ document.getElementById('add-task-button').addEventListener('click', function ()
     addTask();
 })
 
+const changeImg = () => {
+    // document.getElementById('checkbox1').src = '/img/done.png';
+    for (let i = 0; i < allImg.length; i++) {
+             // изминение масства с true false
+            allImg[i].src = '/img/done.png';
+            taskList[i] = taskList[i].replace('notD', 'd');
+            taskList[i] = taskList[i].replace('not ', '');
+            localStorage.setItem('tasks', JSON.stringify(taskList));
+            // location.reload();
+    }
+}
+
 // remove <Li>
 const del = () => {
     for (let i = 1; i < allButton.length; i++) {
         allButton[i].addEventListener('click', function () {
-            checkboxNumber--;
             allLi[i - 1].remove();
             taskList.splice(i - 1, 1);
             localStorage.setItem('tasks', JSON.stringify(taskList));
@@ -75,12 +89,12 @@ const del = () => {
 
 del();
 
-const taskCompleted = () => {
+/*const taskCompleted = () => {
     allInput = document.querySelectorAll('input');
     for (let i = 1; i < allInput.length; i++) {
         checkboxList[i - 1] = allInput[i].checked;
     }
     localStorage.setItem('check', JSON.stringify(checkboxList));
-}
+}*/
 
-setInterval(taskCompleted, 100);
+// setInterval(taskCompleted, 100);
